414f0e3ddd4db6527811d2930c1489eaff0cbe23
skutner <sava.dumitru.daniel@gmail.com>
2020-05-04 22:21:06 +0300

    small refactoring

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (!process.env.PSK_ROOT_INSTALATION_FOLDER) {
     process.env.PSK_ROOT_INSTALATION_FOLDER = path.resolve("." + __dirname + "/../..");
 }
-module.exports.getDossierWizardMiddleware = require("./DossierWizardMiddleware");
+module.exports = require("./DossierWizardMiddleware");
 
