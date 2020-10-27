9ed416cf2331d38fff4ad74a4456388bf28f8e38
Irimia Cosmin-Iulian <irimia.cosmin@gmail.com>
2020-10-27 09:48:54 +0200

    Changed the way that files upload.

--- utils.js
+++ utils.js
@@ -1,125 +1,122 @@
 function bodyParser(req, callback) {
-	let bodyContent = '';
+    let bodyContent = '';
 
-	req.on('data', function (dataChunk) {
-		bodyContent += dataChunk;
-	});
+    req.on('data', function (dataChunk) {
+        bodyContent += dataChunk;
+    });
 
-	req.on('end', function () {
-		req.body = bodyContent;
-		callback(undefined, req.body);
-	});
+    req.on('end', function () {
+        req.body = bodyContent;
+        callback(undefined, req.body);
+    });
 
-	req.on('error', function (err) {
-		callback(err);
-	});
+    req.on('error', function (err) {
+        callback(err);
+    });
 }
 
 function formDataParser(req, callback) {
-	let formData = [];
-	let currentFormItem;
-	let currentBoundary;
-	let dataBuf = Buffer.alloc(0);
-	req.on('data', function (dataChunk){
-		dataBuf = Buffer.concat([dataBuf, dataChunk]);
-	});
+    let formData = [];
+    let currentFormItem;
+    let currentBoundary;
+    let dataBuf = Buffer.alloc(0);
+    let defaultItem = {
+        bufferStartIndex: 0,
+        bufferEndIndex: 0,
+        increaseBothBuffers: (size) => {
+            currentFormItem.bufferStartIndex += size;
+            currentFormItem.bufferEndIndex += size;
+        },
+        increaseEndBuffer: (size) => {
+            currentFormItem.bufferEndIndex += size;
+        }
+    }
 
-	req.on('end', function () {
-		formParser(dataBuf);
-		req.formData = formData;
-		callback(undefined, req.formData);
-	});
+    req.on('data', function (dataChunk) {
+        dataBuf = Buffer.concat([dataBuf, dataChunk]);
+    });
 
-	req.on('error', function (err) {
-		callback(err);
-	});
+    req.on('end', function () {
+        formParser(dataBuf);
+        req.formData = formData;
+        callback(undefined, req.formData);
+    });
 
-	function formParser(data) {
-		data = data.toString();
-		let dataArray = data.split(/[\r\n]+/g);
-		let removeOneLine = false;
-		dataArray.forEach((dataLine)=>{
-			let lineHandled = false;
-			if(dataLine.indexOf('------') === 0){
-				if(typeof currentBoundary === "undefined"){
-					//we got a new boundary
-					currentBoundary = dataLine;
-					lineHandled = true;
-				}else{
-					if(dataLine.indexOf(currentBoundary)+'--' !== -1){
-						//we found a boundary end
-						currentBoundary = undefined;
-						//we add the formItem to formData and consider that is done
-						formData.push(currentFormItem);
-						currentFormItem = undefined;
-						lineHandled = true;
-						removeOneLine = true;
-					}else{
-						//it's just content... we do nothing at this point
-					}
-				}
-			}
-			if(dataLine.indexOf('Content-Disposition:') !== -1){
-				const formItemMeta = dataLine.split("; ");
-				formItemMeta.forEach(meta=>{
-					if(meta.indexOf("name=") !== -1){
-						const itemType = meta.replace("name=", "");
-						currentFormItem = {
-							type: itemType,
-							content: "",
-							ingestContent: function(data){
-								currentFormItem.content += data+'\r\n';
-							}
-						}
-					}
-				});
-				lineHandled = true;
-				removeOneLine = true;
-			}
-			if(dataLine.indexOf('Content-Type:') !== -1){
-				const contentType = dataLine.replace('Content-Type: ', "");
-				switch(currentFormItem.type){
-					case "file":
-						if(contentType.indexOf("text/") !== -1){
-							currentFormItem.content = "";
-							currentFormItem.ingestContent = function(data){
-								currentFormItem.content += data+'\r\n';
-							}
-						}else{
-							currentFormItem.content = [];
-						}
-						break;
-					default:
-						currentFormItem.content = "";
-				}
-				lineHandled = true;
-				removeOneLine = true;
-			}
-			if(!lineHandled){
-				//it's pure content
-				if(typeof currentFormItem !== "undefined"){
-					currentFormItem.ingestContent(dataLine);
-				}
-			}
-		});
-	}
+    req.on('error', function (err) {
+        callback(err);
+    });
+
+    function formParser(data) {
+        let dataAsString = data.toString();
+        let dataArray = dataAsString.split(/[\r\n]+/g);
+
+        currentFormItem = defaultItem;
+        for (let dataLine of dataArray) {
+            let lineHandled = false;
+            if (dataLine.indexOf('------') === 0) {
+                if (typeof currentBoundary === "undefined") {
+                    //we got a new boundary
+                    currentBoundary = dataLine;
+
+                    currentFormItem.increaseBothBuffers(dataLine.length + 2)
+
+                    lineHandled = true;
+                } else if (dataLine.indexOf(currentBoundary) + '--' !== -1) {
+                    //we found a boundary end
+                    currentBoundary = undefined;
+
+                    //Due to encoding method of the characters, in some scenarios we will need to prevent the lose of bytes
+                    //That's why in the final boundary we add them back
+                    currentFormItem.increaseEndBuffer(data.byteLength - dataAsString.length);
+                    currentFormItem = {
+                        type: currentFormItem.type,
+                        content: data.slice(currentFormItem.bufferStartIndex + 2, currentFormItem.bufferEndIndex + 2)
+                    }
+                    //we add the formItem to formData and consider that is done
+                    formData.push(currentFormItem);
+
+                    currentFormItem = defaultItem;
+                    lineHandled = true;
+                }
+            }
+            if (dataLine.indexOf('Content-Disposition:') !== -1) {
+                const formItemMeta = dataLine.split("; ");
+                for (let meta of formItemMeta) {
+                    if (meta.indexOf("name=") === 0) {
+                        currentFormItem.type = meta.replace("name=", "").replace(/\"|'/g, "");
+                        break;
+                    }
+                }
+
+                currentFormItem.increaseBothBuffers(dataLine.length + 2)
+                lineHandled = true;
+            }
+            if (dataLine.indexOf('Content-Type:') !== -1) {
+                currentFormItem.increaseBothBuffers(dataLine.length + 2)
+                lineHandled = true;
+            }
+            if (!lineHandled) {
+                currentFormItem.increaseEndBuffer(dataLine.length + 1)
+            }
+        }
+    }
 }
 
 function redirect(req, res) {
-	const URL_PREFIX = require("./constants").URL_PREFIX;
-	res.statusCode = 303;
-	let redirectLocation = 'index.html';
+    const URL_PREFIX = require("./constants").URL_PREFIX;
+    res.statusCode = 303;
+    let redirectLocation = 'index.html';
 
-	if (!req.url.endsWith('/')) {
-		redirectLocation = `${URL_PREFIX}/` + redirectLocation;
-	}
+    if (!req.url.endsWith('/')) {
+        redirectLocation = `${URL_PREFIX}/` + redirectLocation;
+    }
 
-	res.setHeader("Location", redirectLocation);
-	res.end();
+    res.setHeader("Location", redirectLocation);
+    res.end();
 }
 
 module.exports = {
-	bodyParser,
-	formDataParser,
-	redirect
+    bodyParser,
+    formDataParser,
+    redirect
 }
